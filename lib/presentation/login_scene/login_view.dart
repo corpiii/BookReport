9bcbcf2c38e7582cfcef8324eff244c54e6f8021
corpi <lj845854@gmail.com>
2024-01-16 17:58:32 +0900

    feat: 전체 background 컬러 설정

--- lib/presentation/login_scene/login_view.dart
+++ lib/presentation/login_scene/login_view.dart
@@ -8,6 +8,7 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      backgroundColor: Color(0xFFF7FAF4),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
