e4c1e390b365b7776ed1dcbae7204547bde4e98b
corpi <lj845854@gmail.com>
2024-01-17 10:42:37 +0900

    feat: SessionTitle 위젯 생성

--- lib/presentation/home_scene/home_view.dart
+++ lib/presentation/home_scene/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:book_report/presentation/home_scene/widgets/random_advice.dart';
+import 'package:book_report/presentation/home_scene/widgets/session_title.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -6,16 +7,18 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       backgroundColor: Color(0xFFF7FAF4),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(12.0),
+          padding: EdgeInsets.all(12.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               RandomAdvice(),
-              SizedBox(height: 12.0,),
+              SizedBox(height: 20,),
+              SessionTitle('최근 읽은 책'),
+              SizedBox(height: 10,),
             ],
           ),
         ),
