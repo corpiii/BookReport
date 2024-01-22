e3f7ac77321b93d18fe09e7cbb0b49e51e21a7aa
corpi <lj845854@gmail.com>
2024-01-22 12:35:06 +0900

    feat: HomeViewState 구현 HomeViewModel stateNotifier 적용

--- lib/presentation/home_scene/widgets/random_advice.dart
+++ lib/presentation/home_scene/widgets/random_advice.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class RandomAdvice extends StatelessWidget {
   const RandomAdvice({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: Colors.blue,
-      ),
-      width: MediaQuery.of(context).size.width,
-      height: 150,
-    );
+    return Consumer(builder: (context, ref, child) {
+      return Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.blue,
+        ),
+        width: MediaQuery.of(context).size.width,
+        height: 150,
+        child: Center(child: Text('feawf')),
+      );
+    });
   }
 }
