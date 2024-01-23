79bf81c8315e747071d7519e515e404c292f2bc9
corpi <lj845854@gmail.com>
2024-01-23 10:48:50 +0900

    feat: EditBookUseCase 구현

--- lib/domain/usecase/book/edit_book_use_case/edit_book_use_case_impl.dart
+++ lib/domain/usecase/book/edit_book_use_case/edit_book_use_case_impl.dart
@@ -1,15 +1,17 @@
 import 'package:book_report/domain/dto/book_dto.dart';
 import 'package:book_report/domain/model/app_error.dart';
 import 'package:book_report/domain/model/book.dart';
+
 import 'package:book_report/domain/model/result.dart';
 import 'package:book_report/domain/repository/book_management_repository.dart';
 import 'package:book_report/domain/translator/book_translator.dart';
-import 'package:book_report/domain/usecase/book/delete_book_use_case/interface/delete_book_use_case.dart';
 
-class DeleteBookUseCaseImpl implements DeleteBookUseCase {
+import 'interface/edit_book_use_case.dart';
+
+class EditBookUseCaseImpl implements EditBookUseCase {
   final BookManagementRepository _bookManagementRepository;
 
-  const DeleteBookUseCaseImpl({
+  const EditBookUseCaseImpl({
     required BookManagementRepository bookManagementRepository,
   }) : _bookManagementRepository = bookManagementRepository;
 
@@ -17,7 +19,7 @@ class DeleteBookUseCaseImpl implements DeleteBookUseCase {
   Future<Result<Book>> execute(Book model) async {
     final translator = BookTranslator();
     final dto = translator.translateFrom(model);
-    final result = await _bookManagementRepository.deleteBook(model: dto);
+    final result = await _bookManagementRepository.editBook(model: dto);
 
     switch (result) {
       case Success<BookDTO>():
