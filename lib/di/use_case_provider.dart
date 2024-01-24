c5d0d93a4f32b560d94e61a3dcd2b45be7c23a98
corpi <lj845854@gmail.com>
2024-01-24 15:08:45 +0900

    feat: LogoutUseCaseProvider 생성

--- lib/di/use_case_provider.dart
+++ lib/di/use_case_provider.dart
@@ -1,4 +1,6 @@
 import 'package:book_report/di/repository_provider.dart';
+import 'package:book_report/domain/usecase/logout_use_case/interface/logout_use_case.dart';
+import 'package:book_report/domain/usecase/logout_use_case/logout_use_case_impl.dart';
 import 'package:book_report/domain/usecase/oauth_login_use_case/interface/oauth_login_use_case.dart';
 import 'package:book_report/domain/usecase/oauth_login_use_case/oauth_login_use_case_impl.dart';
 import 'package:book_report/domain/usecase/random_advice_use_case/interface/random_advice_use_case.dart';
@@ -11,4 +13,8 @@ final randomAdviceUseCaseProvider = Provider<RandomAdviceUseCase>((ref) {
 
 final oAuthLoginUseCaseProvider = Provider<OAuthLoginUseCase>((ref) {
   return OAuthLoginUseCaseImpl(oAuthRepository: ref.watch(oAuthLoginRepositoryProvider));
+});
+
+final oauthLogoutUseCaseProvider = Provider<LogoutUseCase>((ref) {
+  return LogoutUseCaseImpl(oAuthRepository: ref.watch(oAuthLoginRepositoryProvider));
 });
