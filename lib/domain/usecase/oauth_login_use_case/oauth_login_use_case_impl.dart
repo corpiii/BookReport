e52de98c87245460efe628a2db5d56344da57684
corpi <lj845854@gmail.com>
2024-01-24 10:47:44 +0900

    chore: 커밋누락

--- lib/domain/usecase/oauth_login_use_case/oauth_login_use_case_impl.dart
+++ lib/domain/usecase/oauth_login_use_case/oauth_login_use_case_impl.dart
@@ -3,7 +3,7 @@ import 'package:book_report/domain/model/oauth_method.dart';
 import 'package:book_report/domain/model/result.dart';
 import 'package:book_report/domain/model/user.dart';
 import 'package:book_report/domain/repository/oauth_repository/google_login_repository.dart';
-import 'package:book_report/domain/usecase/oauth_login_use_case/google_login_use_case.dart';
+import 'package:book_report/domain/translator/user_translator.dart';
 import 'package:book_report/domain/usecase/oauth_login_use_case/interface/oauth_login_use_case.dart';
 
 class OAuthLoginUseCaseImpl implements OAuthLoginUseCase {
@@ -16,12 +16,13 @@ class OAuthLoginUseCaseImpl implements OAuthLoginUseCase {
   @override
   Future<Result<User>> execute(OAuthMethod method) async {
     final Result<UserDTO> result;
+    final translator = UserTranslator();
 
     switch (method) {
       case OAuthMethod.apple:
         // TODO: Handle this case.
       case OAuthMethod.google:
-      result = await _googleLoginRepository.login();
+        result = await _googleLoginRepository.login();
       case OAuthMethod.kakao:
         // TODO: Handle this case.
       default:
@@ -31,11 +32,11 @@ class OAuthLoginUseCaseImpl implements OAuthLoginUseCase {
     switch (result) {
       case Success<UserDTO>():
         final dto = result.data;
-        
+        final user = translator.translateTo(dto);
 
+        return Result.success(user);
       case Error<UserDTO>():
-
+        return Result.error(result.e);
     }
   }
-
 }
