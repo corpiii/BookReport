3629574b487945e3958ab7998268f736f15b9df4
corpi <lj845854@gmail.com>
2024-01-31 17:46:01 +0900

    feat: FetchBookReportListUseCase 정의 및 fetchBookReportList 메서드 구현

--- lib/presentation/books_scene/book_report_list_scene/book_report_list_view_model.dart
+++ lib/presentation/books_scene/book_report_list_scene/book_report_list_view_model.dart
@@ -1,16 +1,40 @@
 import 'package:book_report/domain/model/book.dart';
+import 'package:book_report/domain/model/book_report.dart';
+import 'package:book_report/domain/model/result.dart';
+import 'package:book_report/domain/usecase/book_report/fetch_book_report_list_use_case/interface/fetch_book_report_list_use_case.dart';
 import 'package:book_report/presentation/books_scene/book_report_list_scene/book_report_list_view_state.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class BookReportListViewModel extends StateNotifier<BookReportListViewState> {
   final Book _bookModel;
+  final FetchBookReportListUseCase _fetchBookReportListUseCase;
 
   String get navigationTitle => _bookModel.title;
 
   BookReportListViewModel({
     required Book bookModel,
+    required FetchBookReportListUseCase fetchBookReportListUseCase,
   })  : _bookModel = bookModel,
+        _fetchBookReportListUseCase = fetchBookReportListUseCase,
         super(BookReportListViewState());
 
+  Future<void> fetchBookReportList({
+    required void Function() onComplete,
+    required void Function(String message) onError,
+  }) async {
+    final result = await _fetchBookReportListUseCase.execute();
 
+    switch (result) {
+      case Success<List<BookReport>>():
+        final data = result.data;
+
+        state = state.copyWith(bookReportList: data);
+        onComplete();
+
+        return;
+      case Error<List<BookReport>>():
+        onError(result.e);
+        return;
+    }
+  }
 }
--- lib/presentation/books_scene/book_report_list_scene/book_report_list_view_state.dart
+++ lib/presentation/books_scene/book_report_list_scene/book_report_list_view_state.dart
@@ -1,6 +1,14 @@
 import 'package:book_report/domain/model/book_report.dart';
 
-class BookReportListViewState {
-  final List<BookReport> bookReportList = [];
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+part 'book_report_list_view_state.freezed.dart';
+
+@freezed
+class BookReportListViewState with _$BookReportListViewState {
+  const factory BookReportListViewState({
+    @Default([]) List<BookReport> bookReportList,
+  }) = _BookReportListViewState;
 
 }
